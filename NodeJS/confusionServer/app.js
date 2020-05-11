4359e55b82915175d82477361c6b0ab034df08fa
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-05-11 16:07:28 +0530

    Commit
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- NodeJS/confusionServer/app.js
+++ NodeJS/confusionServer/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 const mongoose = require('mongoose');
 
 const Dishes= require('./models/dishes');
@@ -20,7 +21,14 @@ connect.then((db)=>{
 }, (err)=>{ console.error(err) });
 
 var app = express();
-
+app.all('*',(req,res,next)=>{
+  if(req.secure){
+    return next();
+  }
+  else{
+    res.redirect(307,'https://'+req.hostname+':'+app.get('secPort')+req.url);
+  }
+})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -34,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/img',uploadRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
